7ebdf15062a7400744ac9098675b113c56165aa8
abdullahio1 <102975901+abdullahio1@users.noreply.github.com>
2025-10-02 07:18:39 +0200

    Category class

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+namespace LibraryManagement.Models
+{
+    public class Category
+    {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public int DisplayOrder { get; set;}
+    }
+}
--- obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/LibraryManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d572ebd1e1d28424a50eb451c7841d19487c9eca")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
